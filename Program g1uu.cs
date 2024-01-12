8f271aeb1bfc4c35464249168dbac0c8c4f684cf
abuamira44 <145227273+abuamira44@users.noreply.github.com>
2024-01-12 10:43:15 -0500

    Add files via upload

---
+++ Program g1uu.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Define the IQuittable interface
+public interface IQuittable
+{
+    void Quit();
+}
+
+// Modify the Employee class to inherit from IQuittable and implement the Quit() method
+public class Employee : IQuittable
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Implement the Quit method from the IQuittable interface
+    public void Quit()
+    {
+        Console.WriteLine($"{FirstName} {LastName} has quit the organization.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Instantiate an Employee object
+        Employee employee = new Employee { FirstName = "John", LastName = "Doe" };
+
+        // Use polymorphism to create an object of type IQuittable
+        IQuittable quittableEmployee = employee;
+
+        // Call the Quit method on the IQuittable object
+        quittableEmployee.Quit();
+    }
+}
